90d2d00a88429dd092caa1de4161820c85374d1d
Priya Singh <2k24.cs1m.2414024@gmail.com>
2025-11-30 06:50:21 +0000

    data

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 243102
+let accountEmail = "priya.psit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+let accountCollege;
+
+// accountId=24567     //not allowed
+accountEmail="rajput.psit@gmail.com"
+accountPassword="12321"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountCollege])
+
+
+// prefer not to use var
+// because of issue in block scope and functional scope
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+ "use strict";  //treat all js code as newer version
+//  alert(3+3)  we are using nodeJS, not browser
+console.log(3+3)
+console.log("Priya")
+let name="Priya"
+let age=18
+let isLoggedIn=false
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// symbol => unique
+// object
+console.log(typeof "Priya");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
